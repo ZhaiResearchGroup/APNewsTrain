dd66c850d4b59adde9a109a70cbf9e2a417c969b
Aaron Green <aaron.green281@gmail.com>
2018-02-24 15:29:49 -0600

    train

--- apnews_train.py
+++ apnews_train.py
@@ -3,4 +3,11 @@ from dataset_loader import load_all_sentences
 
 if __name__ == "__main__":
 	sentences = load_all_sentences("corpus/") 
-	print(len(sentences))
+
+	training_corpus = [gensim.models.doc2vec.TaggedDocument(sen, [i]) for (i, sen) in enumerate(sentences)]
+	
+	model = gensim.models.doc2vec.Doc2Vec()
+	model.build_vocab(training_corpus)
+	model.train(training_corpus, total_examples=model.corpus_count, epochs=model.iter)
+
+	model.save("apnews_sen_model.model")
